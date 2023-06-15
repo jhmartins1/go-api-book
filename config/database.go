c4a05e42314425846a09da3a9860579333abe867
João Heitor <78935085+jhmartins1@users.noreply.github.com>
2023-06-14 22:18:23 -0300

    chore: database configuration

---
+++ config/database.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"golang-crud/helper"
+
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "go-crud"
+)
+
+func DatabaseConntection() *sql.DB {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db,err := sql.Open("postgres", sqlInfo)
+	helper.PanicIfError(err)
+
+	err = db.Ping()
+	helper.PanicIfError(err)
+
+	log.Info().Msg("Connected to database")
+
+	return db
+}
--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	fmt.Printf("Start main")
 
 	routes := httprouter.New()
+	routes.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Welcome Home")
+	})
 	server := http.Server{Addr: "localhost:8888", Handler: routes}
 
 	err := server.ListenAndServe()
